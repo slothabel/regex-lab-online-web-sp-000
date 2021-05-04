1c715a1b1a0e6a9a9d0ebf2be265511a5f0fd47d
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-04 23:16:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,8 +24,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-#binding.pry
-if phone.match(/[\d\d\d]-[\d\d\d]-[\d\d\d]/)
+binding.pry
+if phone.match(/[\d+]-[\d+]-[\d+]/)
   true
 else
   false
