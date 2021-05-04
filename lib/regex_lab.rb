b8e4a003feb0028dcb6aea55a366f918239f554e
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-04 23:27:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-binding.pry
+#binding.pry
 if phone.scan(/[\d{3}]-*[\d{3}]-*[\d{4}]/)
   true
 else
